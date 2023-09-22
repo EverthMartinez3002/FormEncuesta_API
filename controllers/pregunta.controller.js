b3eb2f9472b3bb2e6080dcaf6a7a8ab05ad27ab4
EverthMartinez3002 <00156021@uca.edu.sv>
2023-09-22 15:52:31 -0600

    'pregunta-controller-create'

--- controllers/pregunta.controller.js
+++ controllers/pregunta.controller.js
@@ -6,7 +6,7 @@ preguntaController.GetAllByEncuestaId = async (req, res) => {
     try {
         const { encuestaId } = req.params;
         const encuesta = await db.Encuesta.findByPk(encuestaId, {
-          attributes: ['id', 'titulo', 'descripcion', 'usuarioId'],
+            attributes: ['id', 'titulo', 'descripcion', 'usuarioId'],
             include: db.Pregunta,
         });
 
@@ -20,6 +20,25 @@ preguntaController.GetAllByEncuestaId = async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: 'Error al recuperar preguntas' });
+    }
+};
+
+preguntaController.create = async (req, res) => {
+    try {
+        const { preguntas, encuestaId } = req.body;
+
+        if (!encuestaId) {
+            return res.status(400).json({ error: 'No se proporcionó un encuestaId válido.' });
+        }
+
+        const preguntasCreadas = await db.Pregunta.bulkCreate(
+            preguntas.map(pregunta => ({ ...pregunta, encuestaId }))
+        );
+
+        res.status(201).json({ preguntas: preguntasCreadas });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al crear las preguntas.' });
     }
 };
 
--- routes/api/pregunta.router.js
+++ routes/api/pregunta.router.js
@@ -5,6 +5,7 @@ const middlewares = require("../../middlewares/auth.middlewares");
 const preguntaController = require('../../controllers/pregunta.controller');
 
 router.get("/:encuestaId", middlewares.authenticationSession, preguntaController.GetAllByEncuestaId);
+router.post('/create', middlewares.authenticationSession, preguntaController.create);
 
 
 module.exports = router;
